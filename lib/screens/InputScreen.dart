fdd099f73730a7f5a96d6bb1319e92b3b223b233
suyash-03 <suyash.singh9450@gmail.com>
2022-11-19 06:53:10 +0530

    Input

--- lib/screens/InputScreen.dart
+++ lib/screens/InputScreen.dart
@@ -1,4 +1,15 @@
 import 'package:breathalyzer_app/controllers/input_controller.dart';
+import 'package:breathalyzer_app/screens/InputScreens/voc10.dart';
+import 'package:breathalyzer_app/screens/InputScreens/voc12.dart';
+import 'package:breathalyzer_app/screens/InputScreens/voc13.dart';
+import 'package:breathalyzer_app/screens/InputScreens/voc2.dart';
+import 'package:breathalyzer_app/screens/InputScreens/voc3.dart';
+import 'package:breathalyzer_app/screens/InputScreens/voc4.dart';
+import 'package:breathalyzer_app/screens/InputScreens/voc5.dart';
+import 'package:breathalyzer_app/screens/InputScreens/voc6.dart';
+import 'package:breathalyzer_app/screens/InputScreens/voc7.dart';
+import 'package:breathalyzer_app/screens/InputScreens/voc8.dart';
+import 'package:breathalyzer_app/screens/InputScreens/voc9.dart';
 import 'package:breathalyzer_app/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -6,6 +17,9 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:neopop/widgets/buttons/neopop_button/neopop_button.dart';
 import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_sliders/sliders.dart';
+
+import 'InputScreens/voc1.dart';
+import 'InputScreens/voc11.dart';
 
 class InputScreen extends StatefulWidget {
   const InputScreen({Key? key}) : super(key: key);
@@ -23,19 +37,35 @@ class _InputScreenState extends State<InputScreen> {
         return Scaffold(
           backgroundColor: BrandColor.drawerBackgroundColor,
           body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisSize: MainAxisSize.min,
             children: [
               const SizedBox(height: 30,),
               Padding(
-                padding: const EdgeInsets.fromLTRB(0,20,10,10),
+                padding: const EdgeInsets.fromLTRB(20,20,10,10),
                 child: Text("Enter the data for All 13 VOC's \nfrom the sensor",style: GoogleFonts.poppins(fontSize: 22,color: Colors.white,fontWeight: FontWeight.w600),),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20,20,10,0),
+                child: Text("All Values are scaled by /10^6",style: GoogleFonts.poppins(fontSize: 16,color: Colors.white,fontWeight: FontWeight.w400),),
               ),
               Expanded(
                 child: PageView(
                   controller: pageController,
                   children:  [
-                    VOC1(inputController: controller),
-                    VOC1(inputController: controller),
+                    VOC1(inputController: controller,pageController: pageController,),
+                    VOC2(inputController: controller,pageController: pageController,),
+                    VOC3(inputController: controller,pageController: pageController,),
+                    VOC4(inputController: controller,pageController: pageController,),
+                    VOC5(inputController: controller,pageController: pageController,),
+                    VOC6(inputController: controller,pageController: pageController,),
+                    VOC7(inputController: controller,pageController: pageController,),
+                    VOC8(inputController: controller,pageController: pageController,),
+                    VOC9(inputController: controller,pageController: pageController,),
+                    VOC10(inputController: controller,pageController: pageController,),
+                    VOC11(inputController: controller,pageController: pageController,),
+                    VOC12(inputController: controller,pageController: pageController,),
+                    VOC13(inputController: controller,pageController: pageController,),
                   ],
                 ),
               )
@@ -47,96 +77,5 @@ class _InputScreenState extends State<InputScreen> {
   }
 }
 
-class VOC1 extends StatefulWidget {
-  final InputController inputController;
-  const VOC1({Key? key, required this.inputController}) : super(key: key);
 
-  @override
-  State<VOC1> createState() => _VOC1State();
-}
-
-class _VOC1State extends State<VOC1> {
-  List<bool> selectionList = List.filled(19, false);
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      shrinkWrap: true,
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(30.0,20,10,10),
-            child: Text("VOC 1",style: GoogleFonts.poppins(fontSize: 22,color: Colors.white),),
-          ),
-          CheckboxListTile(
-            title: Text(widget.inputController.vocDataList[0].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
-            checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            value: selectionList[0],
-            onChanged: (newValue) {
-              setState(() {
-                selectionList[0] = newValue!;
-              });
-            },
-            controlAffinity: ListTileControlAffinity.leading,
-          ),
-          CheckboxListTile(
-            title: Text(widget.inputController.vocDataList[0].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
-            checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            value: selectionList[0],
-            onChanged: (newValue) {
-              setState(() {
-                selectionList[0] = newValue!;
-              });
-            },
-            controlAffinity: ListTileControlAffinity.leading,
-          ),
-          CheckboxListTile(
-            title: Text(widget.inputController.vocDataList[1].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
-            checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            value: selectionList[1],
-            onChanged: (newValue) {
-              setState(() {
-                selectionList[1] = newValue!;
-              });
-            },
-            controlAffinity: ListTileControlAffinity.leading,
-          ),
-          CheckboxListTile(
-            title: Text(widget.inputController.vocDataList[2].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
-            checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            value: selectionList[2],
-            onChanged: (newValue) {
-              setState(() {
-                selectionList[2] = newValue!;
-              });
-            },
-            controlAffinity: ListTileControlAffinity.leading,
-          ),
-          SizedBox(height: 100,),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40,vertical: 10),
-            child: NeoPopButton(
-              color: Colors.white,
-              onTapUp: () => HapticFeedback.vibrate(),
-              onTapDown: () => HapticFeedback.vibrate(),
-              border: Border.all(
-                color: BrandColor.drawerBackgroundColor,
-                width: 2,
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children:  [
-                    Text("next",style: GoogleFonts.poppins(color: Colors.black),),
-                  ],
-                ),
-              ),
-            ),
-          ),
-
-
-        ],
-      );
-
-  }
-}
 
---
+++ lib/screens/InputScreens/voc13.dart
@@ -0,0 +1,409 @@
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+import 'package:neopop/widgets/buttons/neopop_button/neopop_button.dart';
+import '../../controllers/input_controller.dart';
+import '../../utils/colors.dart';
+
+class VOC13 extends StatefulWidget {
+  final InputController inputController;
+  final PageController pageController;
+  const VOC13({Key? key, required this.inputController,required this.pageController}) : super(key: key);
+
+  @override
+  State<VOC13> createState() => _VOC13State();
+}
+
+class _VOC13State extends State<VOC13> {
+  List<bool> selectionList = List.filled(19, false);
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      shrinkWrap: true,
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(30.0,20,10,10),
+          child: Text("VOC 13",style: GoogleFonts.poppins(fontSize: 22,color: Colors.white),),
+        ),
+        Row(
+          children: [
+            Flexible(
+              child: CheckboxListTile(
+                title: Text(widget.inputController.vocDataList[0].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
+                checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                value: selectionList[0],
+                onChanged: (newValue) {
+                  setState(() {
+                    for(int i=0;i<selectionList.length; i++){
+                      selectionList[i] = false;
+                    }
+                    selectionList[0] = newValue!;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+            ),
+            Flexible(
+              child: CheckboxListTile(
+                title: Text(widget.inputController.vocDataList[1].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
+                checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                value: selectionList[1],
+                onChanged: (newValue) {
+                  setState(() {
+                    for(int i=0;i<selectionList.length; i++){
+                      selectionList[i] = false;
+                    }
+                    selectionList[1] = newValue!;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+            ),
+            Flexible(
+              child: CheckboxListTile(
+                title: Text(widget.inputController.vocDataList[2].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
+                checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                value: selectionList[2],
+                onChanged: (newValue) {
+                  setState(() {
+                    for(int i=0;i<selectionList.length; i++){
+                      selectionList[i] = false;
+                    }
+                    selectionList[2] = newValue!;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Flexible(
+              child: CheckboxListTile(
+                title: Text(widget.inputController.vocDataList[3].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
+                checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                value: selectionList[3],
+                onChanged: (newValue) {
+                  setState(() {
+                    for(int i=0;i<selectionList.length; i++){
+                      selectionList[i] = false;
+                    }
+                    selectionList[3] = newValue!;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+            ),
+            Flexible(
+              child: CheckboxListTile(
+                title: Text(widget.inputController.vocDataList[4].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
+                checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                value: selectionList[4],
+                onChanged: (newValue) {
+                  setState(() {
+                    for(int i=0;i<selectionList.length; i++){
+                      selectionList[i] = false;
+                    }
+                    selectionList[4] = newValue!;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+            ),
+            Flexible(
+              child: CheckboxListTile(
+                title: Text(widget.inputController.vocDataList[5].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
+                checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                value: selectionList[5],
+                onChanged: (newValue) {
+                  setState(() {
+                    for(int i=0;i<selectionList.length; i++){
+                      selectionList[i] = false;
+                    }
+                    selectionList[5] = newValue!;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Flexible(
+              child: CheckboxListTile(
+                title: Text(widget.inputController.vocDataList[6].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
+                checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                value: selectionList[6],
+                onChanged: (newValue) {
+                  setState(() {
+                    for(int i=0;i<selectionList.length; i++){
+                      selectionList[i] = false;
+                    }
+                    selectionList[6] = newValue!;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+            ),
+            Flexible(
+              child: CheckboxListTile(
+                title: Text(widget.inputController.vocDataList[7].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
+                checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                value: selectionList[7],
+                onChanged: (newValue) {
+                  setState(() {
+                    for(int i=0;i<selectionList.length; i++){
+                      selectionList[i] = false;
+                    }
+                    selectionList[7] = newValue!;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+            ),
+            Flexible(
+              child: CheckboxListTile(
+                title: Text(widget.inputController.vocDataList[8].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
+                checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                value: selectionList[8],
+                onChanged: (newValue) {
+                  setState(() {
+                    for(int i=0;i<selectionList.length; i++){
+                      selectionList[i] = false;
+                    }
+                    selectionList[8] = newValue!;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Flexible(
+              child: CheckboxListTile(
+                title: Text(widget.inputController.vocDataList[9].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
+                checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                value: selectionList[9],
+                onChanged: (newValue) {
+                  setState(() {
+                    for(int i=0;i<selectionList.length; i++){
+                      selectionList[i] = false;
+                    }
+                    selectionList[9] = newValue!;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+            ),
+            Flexible(
+              child: CheckboxListTile(
+                title: Text(widget.inputController.vocDataList[10].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
+                checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                value: selectionList[10],
+                onChanged: (newValue) {
+                  setState(() {
+                    for(int i=0;i<selectionList.length; i++){
+                      selectionList[i] = false;
+                    }
+                    selectionList[10] = newValue!;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+            ),
+            Flexible(
+              child: CheckboxListTile(
+                title: Text(widget.inputController.vocDataList[11].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
+                checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                value: selectionList[11],
+                onChanged: (newValue) {
+                  setState(() {
+                    for(int i=0;i<selectionList.length; i++){
+                      selectionList[i] = false;
+                    }
+                    selectionList[11] = newValue!;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+            ),
+          ],
+        ),          Row(
+          children: [
+            Flexible(
+              child: CheckboxListTile(
+                title: Text(widget.inputController.vocDataList[12].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
+                checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                value: selectionList[12],
+                onChanged: (newValue) {
+                  setState(() {
+                    for(int i=0;i<selectionList.length; i++){
+                      selectionList[i] = false;
+                    }
+                    selectionList[12] = newValue!;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+            ),
+            Flexible(
+              child: CheckboxListTile(
+                title: Text(widget.inputController.vocDataList[13].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
+                checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                value: selectionList[13],
+                onChanged: (newValue) {
+                  setState(() {
+                    for(int i=0;i<selectionList.length; i++){
+                      selectionList[i] = false;
+                    }
+                    selectionList[13] = newValue!;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+            ),
+            Flexible(
+              child: CheckboxListTile(
+                title: Text(widget.inputController.vocDataList[14].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
+                checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                value: selectionList[14],
+                onChanged: (newValue) {
+                  setState(() {
+                    for(int i=0;i<selectionList.length; i++){
+                      selectionList[i] = false;
+                    }
+                    selectionList[14] = newValue!;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+            ),
+          ],
+        ),          Row(
+          children: [
+            Flexible(
+              child: CheckboxListTile(
+                title: Text(widget.inputController.vocDataList[15].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
+                checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                value: selectionList[15],
+                onChanged: (newValue) {
+                  setState(() {
+                    for(int i=0;i<selectionList.length; i++){
+                      selectionList[i] = false;
+                    }
+                    selectionList[15] = newValue!;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+            ),
+            Flexible(
+              child: CheckboxListTile(
+                title: Text(widget.inputController.vocDataList[16].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
+                checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                value: selectionList[16],
+                onChanged: (newValue) {
+                  setState(() {
+                    for(int i=0;i<selectionList.length; i++){
+                      selectionList[i] = false;
+                    }
+                    selectionList[16] = newValue!;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+            ),
+            Flexible(
+              child: CheckboxListTile(
+                title: Text(widget.inputController.vocDataList[17].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
+                checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                value: selectionList[17],
+                onChanged: (newValue) {
+                  setState(() {
+                    for(int i=0;i<selectionList.length; i++){
+                      selectionList[i] = false;
+                    }
+                    selectionList[17] = newValue!;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+            ),
+          ],
+        ),          Row(
+          children: [
+            Flexible(
+              child: CheckboxListTile(
+                title: Text(widget.inputController.vocDataList[18].toString(),style: GoogleFonts.poppins(color: Colors.white,fontSize: 15),),
+                checkboxShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                value: selectionList[18],
+                onChanged: (newValue) {
+                  setState(() {
+                    for(int i=0;i<selectionList.length; i++){
+                      selectionList[i] = false;
+                    }
+                    selectionList[18] = newValue!;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40,vertical: 10),
+          child: NeoPopButton(
+            color: Colors.white,
+            onTapUp: (){
+              HapticFeedback.vibrate();
+              widget.pageController.nextPage(duration: Duration(milliseconds: 200), curve: Curves.easeIn);
+            },
+            onTapDown: () => HapticFeedback.vibrate(),
+            border: Border.all(
+              color: BrandColor.drawerBackgroundColor,
+              width: 2,
+            ),
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:  [
+                  Text("next",style: GoogleFonts.poppins(color: Colors.black),),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40,vertical: 10),
+          child: NeoPopButton(
+            color: Colors.black,
+            onTapUp: (){
+              HapticFeedback.vibrate();
+              widget.pageController.previousPage(duration: Duration(milliseconds: 200), curve: Curves.easeIn);
+            },
+            onTapDown: () => HapticFeedback.vibrate(),
+            border: Border.all(
+              color: Colors.black12,
+              width: 2,
+            ),
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:  [
+                  Text("previous",style: GoogleFonts.poppins(color: Colors.white),),
+                ],
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: 100,)
+      ],
+    );
+  }
+}
